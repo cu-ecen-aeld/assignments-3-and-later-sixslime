3c069a3ac69fa686454041ca9d8356081c8286b1
SixSlime <ryan.mm.taylor@gmail.com>
2026-06-12 17:42:59 -0700

    finder.sh

---
+++ finder-app/finder.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Used claude AI to provide a working example, read man pages for relevant commands, then re-wrote implementation. Primarily gained knowledge of 'wc -l' and 'test -ne'
+
+if [ $# -ne 2 ]
+then
+    echo "Two args are required: <filesdir> <searchstr>"
+    exit 1
+fi
+
+filesdir="$1"
+searchstr="$2"
+
+if [ ! -d $filesdir ]
+then
+    echo "$filesdir is not a directory"
+    exit 1
+fi
+
+x=$(find -L $filesdir -type f | wc -l)
+y=$(grep -r $searchstr $filesdir | wc -l)
+
+echo "The number of files are $x and the number of matching lines are $y"
+
+exit 0
