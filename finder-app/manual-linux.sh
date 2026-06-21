64753595bcb315cf9915f90b8aceba2383562584
SixSlime <ryan.mm.taylor@gmail.com>
2026-06-21 13:29:36 -0700

    -

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -61,7 +61,7 @@ fi
 mkdir "${OUTDIR}/rootfs"
 cd "${OUTDIR}/rootfs"
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-mkdir -p user/bin usr/lib usr/sbin
+mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
 # ---
 
