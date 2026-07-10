e4e983fdcd4a590f30bfa61537df420608f9e7eb
SixSlime <ryan.mm.taylor@gmail.com>
2026-07-10 13:56:20 -0700

    -

--- server/prev.h
+++ server/prev.h
@@ -12,6 +12,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define STRERROR strerror(errno)
+#define WRITE_PATH "/var/tmp/aesdsocketdata"
+#define LISTEN_PORT 9000
+
 void recv_send_file(const char *file_path, int socket_fd);
 
 int write_all(int fd, const void *buf, size_t len);
