da6e082574427f445757b8b8f5be64e72873b8f9
SixSlime <ryan.mm.taylor@gmail.com>
2026-08-04 15:39:20 -0700

    read

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -45,6 +45,7 @@ int aesd_release(struct inode *inode, struct file *filp)
     return 0;
 }
 
+// reads one entry at a time.
 ssize_t aesd_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
     ssize_t retval = 0;
@@ -52,9 +53,44 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count, loff_t *f_p
     /**
      * TODO: handle read
      */
+    struct aesd_dev *dev = filp->private_data;
+    struct aesd_circular_buffer *cbuffer = dev->circular_buffer_ptr;
+    if (mutex_lock_interruptible(&dev->lock)) {
+        return -ERESTARTSYS;
+    }
+    // get read entry:
+    size_t read_offset = 0;
+    struct aesd_aesd_buffer_entry *read_entry = aesd_circular_buffer_find_entry_offset_for_fpos(cbuffer, *f_pos, &read_offset);
+    // return if no data available:
+    if (read_entry == NULL) {
+        retval = 0;
+        goto out;
+    }
+    // get read count:
+    size_t read_count = count;
+    size_t max_read = (read_entry->size) - read_offset;
+    if (read_count > max_read) {
+        read_count = max_read;
+    }
+    // fill output buffer:
+    unsigned long uncopied = copy_to_user(buf, read_entry->buffptr[read_offset], read_count);
+    read_count -= uncopied;
+
+    // update circular buffer offset and f_pos:
+    while (&cbuffer->entry[cbuffer->out_offs] != read_entry) {
+        *f_pos -= cbuffer->entry[cbuffer->out_offs].size;
+        cbuffer->out_offs = (cbuffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    // set outs:
+    *f_pos += read_count;
+    retval = read_count;
+    out:
+    mutex_unlock(&dev->lock);
     return retval;
 }
 
+// intentionally does not use/implement fpos.
 ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
                 loff_t *f_pos)
 {
@@ -69,21 +105,28 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
         return -ERESTARTSYS;
     }
 
+    // get sizes:
     size_t old_size = dev->current_entry.size;
     size_t new_size = old_size + count;
 
+    // make new buffer:
     char* new_buffer = kmalloc(new_size);
     if (new_buffer == NULL) {
         retval = -ENOMEM;
         goto out;
     }
-    char* old_buffer = dev->current_entry.buffptr;
 
+    // copy current/old buffer to new buffer:
+    char* old_buffer = dev->current_entry.buffptr;
     if (old_buffer != NULL) {
         memcpy(new_buffer, old_buffer, old_size);
         free(old_buffer);
     };
+
+    // append write to new buffer:
     unsigned long uncopied = copy_from_user(new_buffer[old_size], buf, count);
+
+    // set outs:
     new_size -= uncopied;
     retval = count -= uncopied;
     dev->current_entry.buffptr = new_buffer;
@@ -120,8 +163,6 @@ static int aesd_setup_cdev(struct aesd_dev *dev)
     }
     return err;
 }
-
-
 
 int aesd_init_module(void)
 {
@@ -183,8 +224,6 @@ void aesd_cleanup_module(void)
 
     unregister_chrdev_region(devno, 1);
 }
-
-
 
 module_init(aesd_init_module);
 module_exit(aesd_cleanup_module);
