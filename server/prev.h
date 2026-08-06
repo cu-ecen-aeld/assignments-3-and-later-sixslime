ab1cf24928f5004302eabb01a5b265023f05f94f
SixSlime <ryan.mm.taylor@gmail.com>
2026-08-05 17:05:43 -0700

    fix

--- server/prev.h
+++ server/prev.h
@@ -6,7 +6,7 @@ int write_all(int fd, const void *buf, size_t len);
 
 int send_all(int sockfd, const void *buf, size_t len);
 
-int send_file_back(const char *file_path, int socket_fd);
+int send_file_back(int file_fd, int socket_fd);
 
 int daemonize(void);
 
